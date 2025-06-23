87d72c8bf83f33c18cc1b9f9ff0ad52d0c2260fe
raihahahan <mraihandev@gmail.com>
2025-06-24 02:50:38 +0800

    cpp-10: change flush config

--- src/config.hpp
+++ src/config.hpp
@@ -2,4 +2,4 @@
 
 constexpr const char* WAL_PATH = "data/db.wal";
 constexpr const char* SSTABLE_DIR = "data/segments";
-constexpr const int LSM_FLUSH_THRESHOLD = 6;
+constexpr const int LSM_FLUSH_THRESHOLD = 100;
